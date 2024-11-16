0b3a986d58736e954e9df764d87ea326bc75eb26
Zaid <pilarads06@gmail.com>
2024-11-16 19:47:05 +0100

    se crea componente detalle peli

--- src/app/components/movies/movie-detail/movie-detail.component.ts
+++ src/app/components/movies/movie-detail/movie-detail.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../../../services/movies.service';
+import { Pelicula } from '../../../models/lista-peliculas-response.interface';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrl: './movie-detail.component.css'
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnInit{
 
 constructor(private moviesService:MoviesService){}
+
+  ngOnInit(): void {
+
+  }
+
+
 
 }
