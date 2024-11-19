a660ce02862d9adcb2c4cf893804d4928dc2bbcb
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-11-19 18:15:43 +0100

    Arreglos hechos

--- src/app/services/lista-personas.service.ts
+++ src/app/services/lista-personas.service.ts
@@ -1,9 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {ListaPersonas} from '../models/lista-personas.interfaces';
-import { catchError, Observable, of, switchMap } from 'rxjs';
+import {Observable} from 'rxjs';
 import { DetallePersonaResponse } from '../models/details-personas.interfaces';
 import { CreditosPersonasResponse } from '../models/creditos-personas.interfaces';
+
+
+const HEADERS = {
+  headers: {
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzkyZWExMjZjZWFiYmNhNGZiZGFhMGU3ZTM2OTZjYSIsIm5iZiI6MTczMTY3MjY3MC4wMjY2OSwic3ViIjoiNjczMWJlMDY3ZWYyYzMxZDc4ZWRhYmY5Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.yu0Vz62aRfMDWK5FKDNiUKsrGrvvd_3zh0xhqp87BNI'
+  }
+};
 
 @Injectable({
   providedIn: 'root'
@@ -13,27 +20,20 @@ export class ListaPersonasService {
   constructor(private http: HttpClient) { }
 
   getPersonas(): Observable<ListaPersonas> {
-    return this.http.get<ListaPersonas>(`https://api.themoviedb.org/3/person/popular?&language=es-ES`, {
-      headers: {
-        Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDA1NzdlOGNkMmUyYjU0OWY5NDYxOTU0NTBmZDQ5YiIsIm5iZiI6MTczMTg2NTU0OS45MDU4MzA0LCJzdWIiOiI2NzMxYmYzNjdlZjJjMzFkNzhlZGFjNGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.EDrGv4SuXDqQRkvgTCjqk3GVvAA0zIMTf7CRDK5863w'}`,
-      },
-    });
+    return this.http.get<ListaPersonas>(`https://api.themoviedb.org/3/person/popular?&language=es-ES`, HEADERS);{     
   }
+}
 
   getpersonasId(id: number): Observable<DetallePersonaResponse> {
-    return this.http.get<DetallePersonaResponse>(`https://api.themoviedb.org/3/person/${id}`, {
-      headers: {
-        Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDA1NzdlOGNkMmUyYjU0OWY5NDYxOTU0NTBmZDQ5YiIsIm5iZiI6MTczMTg2NTU0OS45MDU4MzA0LCJzdWIiOiI2NzMxYmYzNjdlZjJjMzFkNzhlZGFjNGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.EDrGv4SuXDqQRkvgTCjqk3GVvAA0zIMTf7CRDK5863w'}`,},
-    });
+    return this.http.get<DetallePersonaResponse>(`https://api.themoviedb.org/3/person/${id}`, HEADERS); {
   }
+}
 
 
   getCreditosId(id: number): Observable<CreditosPersonasResponse> {
-    return this.http.get<CreditosPersonasResponse>(`https://api.themoviedb.org/3/person/${id}/combined_credits?&language=es-ES`, {
-      headers: {
-        Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDA1NzdlOGNkMmUyYjU0OWY5NDYxOTU0NTBmZDQ5YiIsIm5iZiI6MTczMTg2NTU0OS45MDU4MzA0LCJzdWIiOiI2NzMxYmYzNjdlZjJjMzFkNzhlZGFjNGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.EDrGv4SuXDqQRkvgTCjqk3GVvAA0zIMTf7CRDK5863w'}`,},
-    });
+    return this.http.get<CreditosPersonasResponse>(`https://api.themoviedb.org/3/person/${id}/combined_credits?&language=es-ES`, HEADERS); {  
   }
+}
 
 
 }
