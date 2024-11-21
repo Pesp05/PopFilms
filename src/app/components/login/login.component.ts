da4622df920e3c6ea3db67daacd7bfaad2989278
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-11-21 17:16:59 +0100

    Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,6 +9,10 @@ import { AccountService } from '../../services/account.service';
 })
 export class LoginComponent implements OnInit {
 
+  userName = '';
+  userPhoto = '';
+
+
   constructor(
     private authService: AuthService,
     private accountService: AccountService
@@ -17,12 +21,17 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.authService.createSession().subscribe((response) => {
       localStorage.setItem('session_id', response.session_id);
+      console.log('session_id', response.session_id);
+
       this.accountService.getAccountDetails().subscribe((response) => {
-        localStorage.setItem('user_name', response.name);
+        localStorage.setItem('user_name', response.username);
         localStorage.setItem('user_photo', response.avatar.tmdb.avatar_path);
         localStorage.setItem('logged_in', 'true');
-
-        window.location.href = 'http://localhost:4200/login';
+  
+        this.userName = response.username;
+        this.userPhoto = response.avatar.tmdb.avatar_path
+          ? `https://image.tmdb.org/t/p/original${response.avatar.tmdb.avatar_path}`
+          : '';
       });
     });
   }
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
+import { AccountService } from '../../../services/account.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,25 +11,27 @@ export class NavbarComponent  {
 
   userName = '';
   userPhoto = '';
+  isApproved: boolean = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private accountService: AccountService) { }
 
   createRequestToken() {
     this.authService.createRequestToken().subscribe((response) => {
       localStorage.setItem('token', response.request_token);
 
       // STEP 2 de la autenticación en TMDB (firma del token iniciando sesión en TMDB)
-      window.location.href = `https://www.themoviedb.org/authenticate/${response.request_token}?redirect_to=http://localhost:4200/approved/`;
+      window.location.href = `https://www.themoviedb.org/authenticate/${response.request_token}?redirect_to=http://localhost:4200/login/`;
     });
   }
 
   isLoggedIn() {
+    this.isApproved = true;
     return localStorage.getItem('logged_in') === 'true';
   }
 
   logout() {
     localStorage.clear();
-    window.location.href = 'http://localhost:4200';
+    window.location.href = 'http://localhost:4200/home';
   }
 
 }
