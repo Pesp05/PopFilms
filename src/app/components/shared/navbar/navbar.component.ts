4b1fd451af13230b8b8fc59a43bc5f3cf89a5965
ZaidP6 <pilarads06@gmail.com>
2024-11-22 14:30:24 +0100

    offcancas parte 1

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, TemplateRef } from '@angular/core';
 import { AuthService } from '../../../services/authentication/auth.service';
 import { AccountService } from '../../../services/authentication/account.service';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
@@ -49,4 +50,48 @@ export class NavbarComponent {
     localStorage.clear();
     window.location.href = 'http://localhost:4200/home';
   }
+
+  //---------------------------------------------------------------------------------
+
+  private offcanvasService = inject(NgbOffcanvas);
+
+	openEnd(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { position: 'end' });
+	}
+
+	openTop(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { position: 'top' });
+	}
+
+	openBottom(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { position: 'bottom' });
+	}
+
+	openNoBackdrop(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { backdrop: false });
+	}
+
+	openStaticBackdrop(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { backdrop: 'static' });
+	}
+
+	openScroll(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { scroll: true });
+	}
+
+	openNoKeyboard(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { keyboard: false });
+	}
+
+	openNoAnimation(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { animation: false });
+	}
+
+	openCustomBackdropClass(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { backdropClass: 'bg-info' });
+	}
+
+	openCustomPanelClass(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { panelClass: 'bg-info' });
+	}
 }
