b4e7bb4aeb2f7a5e80bf68c1baf9c45d57cdd647
Pesp05 <mendez.espab24@triana.salesianos.edu>
2024-11-28 12:26:49 +0100

    cambios guardados de filtros de peliculas

--- src/app/components/filtros/filtro-peliculas/filtro-peliculas.component.ts
+++ src/app/components/filtros/filtro-peliculas/filtro-peliculas.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Genre } from '../../../models/genre-list-response.interface';
+import { FiltroPeliculasService } from '../../../services/filtro-peliculas.service';
 
 @Component({
   selector: 'app-filtro-peliculas',
   templateUrl: './filtro-peliculas.component.html',
   styleUrl: './filtro-peliculas.component.css'
 })
-export class FiltroPeliculasComponent {
+export class FiltroPeliculasComponent implements OnInit {
 languageFilter: string = '';
 sortBy: string = '';
 
+listaGeneros: Genre[] = [];
+listaGenerosSeleccionados: Genre[] = [];
+
+constructor(private filtroPeliculaService: FiltroPeliculasService) { }
+
   sendFilters() {
     window.location.href = `http://localhost:4200/movies?language=${this.languageFilter}&sortBy=${this.sortBy}`;
+  }
+
+  ngOnInit() {
+    this.filtroPeliculaService.getGenres().subscribe((response) => {
+      this.listaGeneros = response.genres;
+    });
+  }
+
+  gestionarChecked() {
+    if(document.getElementsByClassName('no-selected')){
+
+    }
   }
 
 }
